42b8e6e75b81b06044c3d0b90789a59ce83a1ecd
Константин <45256837+ConSmit@users.noreply.github.com>
2019-01-16 13:07:51 +0300

    внес в download.sh изменения
    взял код с https://infostart.ru/public/817947/  и внес в download.sh для личного использования

--- download.sh
+++ download.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-set -e
 
-if [ -z "$USERNAME" ]
-then
+USERNAME=
+PASSWORD=
+
+if [[ -z "$USERNAME" ]];then
     echo "USERNAME not set"
     exit 1
 fi
 
-if [ -z "$PASSWORD" ]
-then
+if [[ -z "$PASSWORD" ]];then
     echo "PASSWORD not set"
     exit 1
 fi
 
-if [ -z "$VERSION" ]
-then
-    echo "VERSION not set"
-    exit 1
-fi
+echo "Getting versions, please wait."
 
 SRC=$(curl -c /tmp/cookies.txt -s -L https://releases.1c.ru)
+
 ACTION=$(echo "$SRC" | grep -oP '(?<=form method="post" id="loginForm" action=")[^"]+(?=")')
 EXECUTION=$(echo "$SRC" | grep -oP '(?<=input type="hidden" name="execution" value=")[^"]+(?=")')
 
@@ -34,29 +31,52 @@ curl -s -L \
     --data-urlencode "password=$PASSWORD" \
     https://login.1c.ru"$ACTION"
 
-if ! grep -q "TGC" /tmp/cookies.txt
-then
+
+if ! grep -q "TGC" /tmp/cookies.txt ;then
     echo "Auth failed"
     exit 1
 fi
 
+clear
+
+curl -s -b /tmp/cookies.txt https://releases.1c.ru/project/Platform83 |
+
+    grep 'a href="/version_files?nick=Platform83' |
+    tr -s '="  ' ' ' |
+    awk -F ' ' '{print $5}' |
+    sort -Vr | pr -T -5
+
+read -i "8.3." -p "Input version for download: " -e VER
+
+if [[ -z "$VER" ]];then
+    echo "VERSION not set"
+    exit 1
+fi
+
+if [[ "8.3." = "$VER" ]];then
+    echo "Need full VERSION number"
+    exit 1
+fi
+
+VER1=${VER//./_}
+
 CLIENTLINK=$(curl -s -G \
     -b /tmp/cookies.txt \
     --data-urlencode "nick=Platform83" \
-    --data-urlencode "ver=$VERSION" \
-    --data-urlencode "path=Platform\\${VERSION//./_}\\client_${VERSION//./_}.deb64.tar.gz" \
+    --data-urlencode "ver=$VER" \
+    --data-urlencode "path=Platform\\$VER1\\client_$VER1.deb64.tar.gz" \
     https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')
 
-SERVERINK=$(curl -s -G \
+SERVERLINK=$(curl -s -G \
     -b /tmp/cookies.txt \
     --data-urlencode "nick=Platform83" \
-    --data-urlencode "ver=$VERSION" \
-    --data-urlencode "path=Platform\\${VERSION//./_}\\deb64_${VERSION//./_}.tar.gz" \
-    https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')    
+    --data-urlencode "ver=$VER" \
+    --data-urlencode "path=Platform\\$VER1\\deb64_$VER1.tar.gz" \
+    https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')
 
 mkdir -p dist
 
-curl --fail -b /tmp/cookies.txt -o dist/client.tar.gz -L "$CLIENTLINK"
-curl --fail -b /tmp/cookies.txt -o dist/server.tar.gz -L "$SERVERINK"
+curl --fail -b /tmp/cookies.txt -o dist/${VER}_client64.tar.gz -L "$CLIENTLINK"
+curl --fail -b /tmp/cookies.txt -o dist/${VER}_server64.tar.gz -L "$SERVERLINK"
 
 rm /tmp/cookies.txt
