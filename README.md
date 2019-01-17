cdddf9f48f6edd64f22ac601505f55721fead7ab
Константин Ковалёв <konsmith@yandex.ru>
2019-01-17 18:00:25 +0300

    Убрал, то что мне не надо. Добавил интерактивную авторизацию.

--- README.md
+++ README.md
@@ -1,28 +1,10 @@
 # onec_dock
 
-Скрипт сборки docker контейнера с толстым клиентом 1С
+Скрипт закачки дистрибутива 1С для Debian
 
 ## Использование
 
 - Клонируем этот репозитарий
-- `onec_dock.sh`
+- `download.sh`
 
-Для неинтерактивного запуска необходимо объявить окружения USERNAME, PASSWORD и VERSION, содержащие логин/пароль к [сайту с дистрибутивами](http://releases.1c.ru) 
-и версию платформы соответственно.
-
-```
-export USERNAME=vasya
-export PASSWORD=pupkin
-export VERSION=8.3.8.2167
-./download.sh
-./build.sh
-```
-
-Если необходимые дистрибутивы платформы скачаны вручную и лежат в каталоге `dist`, то для сборки достаточно выполнить `./build.sh --no-tag`.
-
-Полученный образ можно использовать в CI, либо для [запуска GUI](http://infostart.ru/public/548179/) 1С в вашем любимом linux дистрибутиве.
-
-Пример создания чистой БД в текущем каталоге:
-```
-docker run --rm -it -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -v $(pwd):/pwd --user $(id -u) infactum/onec_thick_client bash -c 'xstart; /opt/1C/v8.3/i386/1cv8 CREATEINFOBASE File="/pwd/DB"'
-```
+Имя пользователя, пароль, версия вводится интерактивно
--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-USERNAME=
-PASSWORD=
+read -p "login on login.1c.ru:" USERNAME
+read -p "password:" PASSWORD
 
 if [[ -z "$USERNAME" ]];then
     echo "USERNAME not set"
@@ -67,16 +67,16 @@ CLIENTLINK=$(curl -s -G \
     --data-urlencode "path=Platform\\$VER1\\client_$VER1.deb64.tar.gz" \
     https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')
 
-SERVERLINK=$(curl -s -G \
-    -b /tmp/cookies.txt \
-    --data-urlencode "nick=Platform83" \
-    --data-urlencode "ver=$VER" \
-    --data-urlencode "path=Platform\\$VER1\\deb64_$VER1.tar.gz" \
-    https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')
+#SERVERLINK=$(curl -s -G \
+#    -b /tmp/cookies.txt \
+#    --data-urlencode "nick=Platform83" \
+#    --data-urlencode "ver=$VER" \
+#    --data-urlencode "path=Platform\\$VER1\\deb64_$VER1.tar.gz" \
+#    https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')
 
 mkdir -p dist
 
 curl --fail -b /tmp/cookies.txt -o dist/${VER}_client64.tar.gz -L "$CLIENTLINK"
-curl --fail -b /tmp/cookies.txt -o dist/${VER}_server64.tar.gz -L "$SERVERLINK"
+#curl --fail -b /tmp/cookies.txt -o dist/${VER}_server64.tar.gz -L "$SERVERLINK"
 
 rm /tmp/cookies.txt
