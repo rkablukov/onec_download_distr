56aa421da03a6d591444e082851d407c232d4e90
Каблуков Роман Евгеньевич <roof@altair-2.local>
2020-02-27 17:08:54 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# останавливаем сервер 1с
+service srv1cv83 stop
+
 # распаковываем архив
 rm -r server64 && mkdir server64 && tar xzvf dist/server64.tar.gz -C server64
 
 # устанавливаем сервер 1с
 dpkg -i server64/1c*.deb
 
-# стартуем сервер
+# стартуем сервер 1с
 service srv1cv83 start
