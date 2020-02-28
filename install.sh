c0ff9b6aa8ca49f31a65a6e3320381708b54acdb
Каблуков Роман Евгеньевич <roof@altair-2.local>
2020-02-28 12:30:45 +0300

    fix install script

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # останавливаем сервер 1с
-service srv1cv83 stop
+if service_exists srv1cv83; then
+    service srv1cv83 stop
+fi
 
 # распаковываем архив
-rm -r server64 && mkdir server64 && tar xzvf dist/server64.tar.gz -C server64
+if [ -d server64 ]; then
+    rm -r server64/*
+else
+    mkdir server64
+fi
+tar xzvf dist/server64.tar.gz -C server64
 
 # устанавливаем сервер 1с
 dpkg -i server64/1c*.deb
